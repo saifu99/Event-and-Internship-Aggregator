7fe55d0b3df2317d6da54e6081e6b93e218ac73c
saifu99 <trksaifuyt@gmail.com>
2025-10-26 00:57:38 +0530

    services added

---
+++ frontend/src/services/authService.js
@@ -0,0 +1,20 @@
+import { api } from "../utils/APIRoutes";
+
+export const registerUser = async (userData) => {
+  const res = await api.post("/auth/register", userData);
+  return res.data;
+};
+
+export const loginUser = async (credentials) => {
+  const res = await api.post("/auth/login", credentials);
+
+  if (res.data.token) {
+    localStorage.setItem("token", res.data.token);
+  }
+  return res.data;
+};
+
+export const getProfile = async () => {
+  const res = await api.get("/auth/profile");
+  return res.data;
+};
