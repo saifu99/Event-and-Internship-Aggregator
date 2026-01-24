7302ee456bc9095fcf56c101c3ee434ed833f1a7
saifu99 <trksaifuyt@gmail.com>
2026-01-24 09:56:56 +0530

    cron updated

--- backend/cron/internshala.scrapeJobs.js
+++ backend/cron/internshala.scrapeJobs.js
@@ -1,14 +1,17 @@
 import cron from "node-cron";
 import { scrapeInternshala } from "../scrapers/internshala.scraper.js";
 
-// run once on startup (TEMPORARY)
-scrapeInternshala();
-
+//Daily at 2 AM IST
 cron.schedule(
   "0 2 * * *",
   async () => {
-    await scrapeInternshala();
+    try {
+      console.log("Starting Internshala scrape...");
+      await scrapeInternshala(); // will process all new internships on the page
+      console.log("Internshala scrape finished");
+    } catch (err) {
+      console.error("Internshala scraping failed:", err.message);
+    }
   },
   { timezone: "Asia/Kolkata" }
 );
-
