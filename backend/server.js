fe2673af86d177e463d07b4efafccb28e527a6e3
saifu99 <trksaifuyt@gmail.com>
2026-02-21 01:29:33 +0530

    code updated

--- backend/server.js
+++ backend/server.js
@@ -37,4 +37,4 @@ app.use("/api/internships", internshipRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
